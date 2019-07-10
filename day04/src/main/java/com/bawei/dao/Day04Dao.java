bb2280ea0988628352843403a19f5c06b57f4a7f
靳博文 <1378317625@qq.com>
2019-07-10 11:24:28 +0800

    柴伟沙雕

---
+++ day04/src/main/java/com/bawei/dao/Day04Dao.java
@@ -0,0 +1,11 @@
+package com.bawei.dao;
+
+public class Day04Dao {
+	int a = 5;
+	int b = 3;
+	
+	public void jiafa(){
+		int c = a+b;
+		System.out.println("5+3等于"+c);
+	}
+}
---
+++ day04/src/main/java/com/bawei/dao/Main.java
@@ -0,0 +1,8 @@
+package com.bawei.dao;
+
+public class Main {
+	public static void main(String[] args) {
+		Day04Dao dao = new Day04Dao();
+		dao.jiafa();
+	}
+}
